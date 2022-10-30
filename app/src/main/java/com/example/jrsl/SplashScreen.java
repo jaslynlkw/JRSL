e323b0acdbc00507514d43bb4d1edd3abb81db92
Jaslyn Lau <jaslyn.lkw@gmail.com>
2022-10-30 14:28:55 +0800

    initial commit

---
+++ app/src/main/java/com/example/jrsl/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.example.jrsl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+    private ProgressBar splashProgressBar;
+    private Timer timer;
+    private int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        splashProgressBar = findViewById(R.id.progressBarSplashScreen);
+        timer = new Timer();
+
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (i<100) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                        }
+                    });
+                    splashProgressBar.setProgress(i);
+                    i++;
+                } else {
+                    timer.cancel();
+                    Intent intent = new Intent(SplashScreen.this, Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 0, 30);
+
+    }
+}
