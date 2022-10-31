8b7c00af0b5f00994520265e9c5829f00946ed17
Jaslyn Lau <jaslyn.lkw@gmail.com>
2022-10-31 19:00:42 +0800

    slight changes, ignore

--- app/src/main/java/com/example/jrsl/Login.java
+++ app/src/main/java/com/example/jrsl/Login.java
@@ -2,13 +2,28 @@ package com.example.jrsl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            // upon clicking login btn, redirected to home page
+            case R.id.btnLogin:
+                Intent i = new Intent(this, Home.class);
+                startActivity(i);
+                break;
+        }
+    }
+
 }
