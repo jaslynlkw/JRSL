ac69c9befd66a620c6a29f9527aeefafed6afef5
DESKTOP-QQRB065\shiri <shirinruth6@gmail.com>
2023-01-10 17:29:23 +0800

    Recycler for Saved

--- app/src/main/java/com/example/jrsl/Saved.java
+++ app/src/main/java/com/example/jrsl/Saved.java
@@ -8,17 +8,56 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
 public class Saved extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private ArrayList<CartItem> cartItems = new ArrayList<>();
     public Saved(){
         // require a empty public constructor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.activity_saved, container, false);
+        setUIRef(view);
+        bindCountriesData();
+        return view;
 
-        return inflater.inflate(R.layout.activity_saved, container, false);
+
+    }
+    private void setUIRef(View container)
+    {
+        //Reference of RecyclerView
+        mRecyclerView = container.findViewById(R.id.myCartList);
+        //Linear Layout Manager
+        LinearLayoutManager linearLayoutManager = new GridLayoutManager(getActivity(), 2);
+        //Set Layout Manager to RecyclerView
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+
+        //Create adapter
+        CartItemArrayAdapter myRecyclerViewAdapter = new CartItemArrayAdapter(cartItems, new CartItemArrayAdapter.CartClickListener() {
+            @Override
+            public void onItemClicked(CartItem cartItem) {
+                Toast.makeText(getActivity(), cartItem.getDesc(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //Set adapter to RecyclerView
+        mRecyclerView.setAdapter(myRecyclerViewAdapter);
     }
 
+    private void bindCountriesData()
+    {
+        cartItems.add(new CartItem( "Darcy Collection","White Dress", 25, R.drawable.outfit2));
+        cartItems.add(new CartItem( "Cancy Collection","Brown Coat", 65, R.drawable.outfit3));
+    }
 }
---
+++ app/src/main/java/com/example/jrsl/SavedItemArrayAdapter.java
@@ -0,0 +1,96 @@
+package com.example.jrsl;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SavedItemArrayAdapter extends RecyclerView.Adapter<SavedItemArrayAdapter.MyViewHolder>{
+    private ArrayList<CartItem> myCart;
+    private SavedItemArrayAdapter.SavedClickListener mySavedClickListener;
+
+    public SavedItemArrayAdapter(ArrayList<CartItem> myCart, SavedItemArrayAdapter.SavedClickListener mySavedClickListener){
+        this.myCart = myCart;
+        this.mySavedClickListener = mySavedClickListener;
+    }
+
+    @NonNull
+    @Override
+    public SavedItemArrayAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Inflate RecyclerView row
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_cartitem, parent, false);
+
+        //Create View Holder
+        final SavedItemArrayAdapter.MyViewHolder myViewHolder = new SavedItemArrayAdapter.MyViewHolder(view);
+
+        //Item Clicks
+        myViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mySavedClickListener.onItemClicked(myCart.get(myViewHolder.getLayoutPosition()));
+            }
+        });
+
+        return myViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SavedItemArrayAdapter.MyViewHolder holder, int position) {
+
+        //Set brand
+        holder.savedBrand.setText(myCart.get(position).getBrand());
+
+        //Set desc
+        holder.savedDesc.setText(myCart.get(position).getDesc());
+
+        //Set price
+        String price = "$" + myCart.get(position).getPrice();
+        holder.savedPrice.setText(price);
+
+        // Set image
+        holder.savedImage.setImageResource(myCart.get(position).getImage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return myCart.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    //RecyclerView View Holder
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView savedBrand;
+        private TextView savedDesc;
+        private TextView savedPrice;
+        private ImageView savedImage;
+
+        MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            savedBrand = itemView.findViewById(R.id.CartBrand);
+            savedDesc = itemView.findViewById(R.id.CartDesc);
+            savedPrice = itemView.findViewById(R.id.CartPrice);
+            savedImage = itemView.findViewById(R.id.cartPicture);
+        }
+    }
+
+    //RecyclerView Click Listener
+    public interface SavedClickListener{
+        void onItemClicked(CartItem savedItem);
+    }
+}
